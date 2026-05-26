c2750b05ff70ae9e60dcccea1150ee1af50afe30
foxplort <rchemyakyn@gmail.com>
2026-05-25 23:44:51 -0600

    updated mobile contrast

--- okanerun/src/data/objectData/ground.lua
+++ okanerun/src/data/objectData/ground.lua
@@ -20,13 +20,26 @@ return {
   render = function(self, isEditor)
       local c = (mobileContrastStatus or fore.data.phone) and {25, 30, 45} or {15, 20, 28}
       local ca = {c[1], c[2], c[3], 30}
+
+      local drawOutline = function()
+        fore.graphics.rect(self.x, self.y, self.w, self.h, {0,0,0,63}, false)
+        fore.graphics.rect(self.x-1, self.y-1, self.w+2, self.h+2, {0,0,0,63}, false)
+      end
       
       local drawMain = function()
           fore.graphics.rect(self.x, self.y, self.w, self.h, c)
-          for gx = self.x, self.x + self.w-1, 40 do
-              for gy = self.y, self.y + self.h-1, 40 do
-                  fore.graphics.rect(gx, gy, 1, 1, {150, 200, 255, 20})
-              end
+          if not (mobileContrastStatus or fore.data.phone) then
+            for gx = self.x, self.x + self.w-1, 40 do
+                for gy = self.y, self.y + self.h-1, 40 do
+                    fore.graphics.rect(gx, gy, 1, 1, {150, 200, 255, 20})
+                end
+            end
+          else
+            for gx = self.x, self.x + self.w-2, 40 do
+                for gy = self.y, self.y + self.h-2, 40 do
+                    fore.graphics.rect(gx, gy, 2, 2, {150, 200, 255, 15})
+                end
+            end
           end
       end
 
@@ -42,7 +55,10 @@ return {
           drawDec()
       else
           fore.queuer.submit(L.FLOOR, self.y, drawMain)
-          fore.queuer.submit(L.FLOOR_DEC, self.y + self.w, drawDec)
+          fore.queuer.submit(L.FLOOR_DEC, self.y, drawDec)
+          if (mobileContrastStatus or fore.data.phone) then
+            fore.queuer.submit(L.FLOOR_DEC, self.y + 10000, drawOutline)
+          end
       end
   end
 }
